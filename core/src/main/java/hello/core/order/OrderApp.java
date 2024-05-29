c8dc372a791afc8fd3e9df9065d0577603de94ec
minjikimkim2222 <olivia0929@naver.com>
2024-05-29 09:53:49 +0900

    0529_김영한_2_할인정책, 주문 도메인 개발과 테스트

---
+++ core/src/main/java/hello/core/order/OrderApp.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "member1", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "item1", 10000);
+
+        System.out.println("orderService : " + order);
+        System.out.println("orderService : " + order.calculatePrice());
+
+    }
+}
